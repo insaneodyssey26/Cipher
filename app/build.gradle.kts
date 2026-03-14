1b668f1da55ba9ede4708e0e365b1e5bd7a3705e
insaneodyssey26 <hydrapikachu29@gmail.com>
2026-03-14 14:18:14 +0530

    Added google sans font files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui.text.google.fonts)
     implementation(libs.androidx.material.icons.extended)
     implementation("androidx.appcompat:appcompat:1.7.0")
 
